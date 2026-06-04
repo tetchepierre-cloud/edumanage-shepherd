5d7db49088560e8b2a2da3d3410fe845d5747103
tetchepierre-cloud <tetchepierre@gmail.com>
2026-06-04 23:20:51 +0000

    fix: force dev port 5173 in vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5174, // Fixe le port à 5174 pour Shepherd Mirror Academy
+    port: 5174, // Fixe le port à 5174 pour Shepherd Mirrors Academy
     strictPort: true, // Force Vite à utiliser ce port ou à lever une erreur s'il est pris
   },
 })
