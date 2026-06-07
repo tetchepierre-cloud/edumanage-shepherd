60ae7af2965c79bdd3ee0cce62f79c8f6f9da5c3
tetchepierre-cloud <tetchepierre@gmail.com>
2026-06-07 15:12:34 +0000

    remove port from vite.config, use .env.local

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5174, // Fixe le port à 5174 pour Shepherd Mirrors Academy
-    strictPort: true, // Force Vite à utiliser ce port ou à lever une erreur s'il est pris
+    strictPort: true,
   },
 })
